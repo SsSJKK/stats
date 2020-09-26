71adc9d7c8f5a67e4d8df0b8b99c091bd0b4016a
SsS_JKK <kobil07@mail.ru>
2020-09-27 00:09:09 +0500

    PerDyc

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -35,3 +35,28 @@ func Test_1(t *testing.T) {
 		t.Errorf("invalid result, ex[ected: %v, actual: %v", expected, result)
 	}
 }
+
+func Test_2(t *testing.T) {
+	first := map[types.Category]types.Money{
+		"1": 2,
+		"2": 3,
+		"3": 3,
+	}
+	second := map[types.Category]types.Money{
+		"1": 2,
+		"2": 3,
+		"4": 2,
+	}
+
+	result := PeriodsDynamic(first, second)
+	expected := map[types.Category]types.Money{
+		"1": 0,
+		"2": 0,
+		"3": -3,
+		"4": 2,
+	}
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, ex[ected: %v, actual: %v", expected, result)
+	}
+}
